c275458f2c14b0784d976f162f997c317dbc9505
Den-Zel1 <box1693@bk.ru>
2026-02-06 23:40:04 +0300

    create tables postgreSQL, added endpoits get and post

--- models.py
+++ models.py
@@ -1,2 +1,17 @@
+from sqlalchemy import Integer, String, Column
+from sqlalchemy.orm import declarative_base
 
-from pydantic import BaseModel
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    password_hash = Column(String)
+
+
+class Trash(Base):
+    __tablename__ = "trash"
+    id = Column(Integer, primary_key=True)
+    content = Column(String)
